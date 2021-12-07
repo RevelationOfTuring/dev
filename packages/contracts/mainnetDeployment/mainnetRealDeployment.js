6fb9cff77e9b0444f9376281624ff2307c85a661
MichaelWang <wangguanyi@web3.com>
2021-12-07 15:31:39 +0800

    annotation of deploymentParams.mainnet.js

--- packages/contracts/mainnetDeployment/mainnetRealDeployment.js
+++ packages/contracts/mainnetDeployment/mainnetRealDeployment.js
@@ -1,10 +1,14 @@
+// 部署的具体逻辑都在./mainnetDeployment.js文件中
 const { mainnetDeploy } = require('./mainnetDeployment.js')
+// 导入部署涉及到的一些系统配置及参数
 const configParams = require("./deploymentParams.mainnet.js")
 
+// 部署脚本的main函数定义
 async function main() {
   await mainnetDeploy(configParams)
 }
 
+// main函数的执行
 main()
   .then(() => process.exit(0))
   .catch(error => {
